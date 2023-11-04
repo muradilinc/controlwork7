227aaeeca147acfdc72d9dce293c1e6efa13e167
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-11-04 12:08:22 +0600

    initial project and install Tailwind CSS, created Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="text-center border py-2">
+      <h1 className="text-2xl">Muradil's fast food</h1>
+    </div>
+  );
+};
+
+export default Header;
